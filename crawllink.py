52e25826f43ff9d11414941928582fea46f47926
Singh, Harinder <Harinder.Singh@in.unisys.com>
2018-03-27 16:00:07 +0530

    modularising and adding basic crawl ability

--- crawllink.py
+++ crawllink.py
@@ -1,8 +1,12 @@
 import logging
 import datetime
 import os
+import requests
+from bs4 import BeautifulSoup
 
-def log_crawllink():
+MAX_DEPTH = 5
+
+def crawllink_log():
     """
         Setup logging for scraplink
         Creates a logfile with scraplink_datetime.txt format with DEBUG 
@@ -24,28 +28,64 @@ def log_crawllink():
     logger.setLevel(logging.DEBUG)
     return logger
 
-def main():
-    logger = log_crawllink()
-    
-
+def crawl_link_collector(soup, logger):
     """
-        Summary line.
-
-        Extended description of function.
+        Function used to crawl webpages
 
         Parameters
         ----------
-        arg1 : int
-            Description of arg1
-        arg2 : str
-            Description of arg2
+        soup : BeautifulSoup object
+            used to perform operations on current webpage contents
+        logger : Log object 
+            used for logging events
 
         Returns
         -------
-        int
-            Description of return value
-
+        forward_links[]
+            List of web urls to be crawled further
     """
+    # Pull all instances of <a> tag within page
+    link_lists = soup.find_all('a')
+    # Create for loop to filter the links to sections within page
+    forward_links = []
+    for item in link_lists:
+        temp_link = item.get('href')
+        if ('http' in temp_link or 'www' in temp_link):
+            forward_links.append(item.get('href'))     
+
+    return forward_links
+
+def crawl_spider(seed_url, maxDepth, logger):
+    """
+        Function used to crawl webpages
+
+        Parameters
+        ----------
+        url : string
+            Base url that will be the seed url
+        maxDepth : integer
+            Max depth till where the crawler will work and stop thereafter
+        logger : Log object used for logging events
+
+        Returns
+        -------
+        Null
+    """
+    if maxDepth:
+        logger.debug("Base URL = %s and MaxDepth = %d.", seed_url, maxDepth)
+    else:
+        maxDepth = MAX_DEPTH
+        logger.debug("Base URL = %s and MaxDepth = %d.", seed_url, maxDepth)
+    page = requests.get(seed_url)
+    logger.debug("Response on hitting %s %s ",seed_url, page.status_code)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    forward_links = crawl_link_collector(soup, logger)
+    logger.debug("Web urls obtained on hitting ", seed_url)
+
+def main():
+    logger = crawllink_log()
+    logger.debug("Logger is set up")
+    crawl_spider("http://python.org", 1, logger)
 
 if __name__ == "__main__":
     # execute only if run as a script
